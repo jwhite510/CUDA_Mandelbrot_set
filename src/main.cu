bb9a459158d2bfa1c5b6a9f43f3d9e4a09e0eb79
jonathon <jonathonwhite@protonmail.com>
2021-06-23 15:18:47 -0400

    function runs on gpu, sets some values

--- src/main.cu
+++ src/main.cu
@@ -178,9 +178,14 @@ struct Application
   }
 };
 
-__device__
-void increment_values() {
-
+__global__
+void increment_values(double* d_arr, int n) {
+  int index = blockIdx.x*blockDim.x+threadIdx.x;
+  int stride = blockDim.x*gridDim.x;
+  for(int thread_i = index; thread_i < n; thread_i += stride) {
+    // d_arr[thread_i] = blockIdx.x;
+    d_arr[thread_i] = d_arr[thread_i] + 1;
+  }
 }
 
 
@@ -189,20 +194,31 @@ int main()
   cout<<"hello main is running"<<endl;
   // copy an array to the server and back to host
 
-  int n = 10; // length of the array
+  int n = 20; // length of the array
   // allocate memory on the host
   double *h_arr = new double[n];
   for (int i = 0; i < n; i++) {
     h_arr[i] = i;
   }
+
+  // print before
+  for (int i = 0; i < n; i++) {
+    printf("%-5i ",(int)h_arr[i]);
+  }cout<<endl;
+
   double *d_arr; // allocate on device
   cudaMalloc(&d_arr, n*sizeof(double)); // allocate memory on the device
 
   // copy to the device
   cudaMemcpy(d_arr, h_arr, n*sizeof(double), cudaMemcpyHostToDevice);
 
+  int blocksize = 3;
+  int numBlocks = 3;
+  increment_values<<<numBlocks,blocksize>>>(d_arr, n);
+
   // copy to the host
   cudaMemcpy(h_arr, d_arr, n*sizeof(double), cudaMemcpyDeviceToHost);
+  // print after
   for (int i = 0; i < n; i++) {
     printf("%-5i ",(int)h_arr[i]);
   }cout<<endl;
